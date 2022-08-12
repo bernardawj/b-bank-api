9fd3a3d8bf18aabbeb6a1c1a6ebc1136476057cc
Bernard Ang <work@bernardawj.com>
2022-08-12 23:34:28 +0800

    Initial commit

---
+++ src/main/java/com/bernardawj/bbank/BBankApplication.java
@@ -0,0 +1,13 @@
+package com.bernardawj.bbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bernardawj/bbank/BBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernardawj.bbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
